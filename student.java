dc6f2785e29efbdc966756c4f9ab42e10cc739fc
Veeravajhula Sai Mathur <51848442+vsaimathur@users.noreply.github.com>
2022-06-10 00:09:46 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,23 @@
+package library;
+
+import java.util.Scanner;    
+public class student {
+
+String studentName;
+String regNum;
+
+book borrowedBooks[] = new book[3];
+public int booksCount = 0;
+
+Scanner input = new Scanner(System.in);
+
+public student(){
+
+    System.out.println("Enter Student Name:");
+    this.studentName = input.nextLine();
+
+    System.out.println("Enter Reg Number:");
+    this.regNum = input.nextLine();
+
+}
+}
